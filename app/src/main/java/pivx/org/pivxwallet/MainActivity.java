efeae7e28cc7c484c1a68d38b9eeda831153b0ca
Gregory Solarte <Neoperol@Gregorys-MacBook-Pro.local>
2017-05-01 16:05:17 -0400

    qr button aded

--- app/src/main/java/pivx/org/pivxwallet/MainActivity.java
+++ app/src/main/java/pivx/org/pivxwallet/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_qr) {
             return true;
         }
 
