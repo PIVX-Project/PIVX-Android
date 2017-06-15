4abf27598eb1663ea74b77d2475ee230d1a04bc8
furszy <mati_fur@hotmail.com>
2017-06-15 18:23:12 -0300

    SnappyStore android

---
+++ app/src/androidTest/java/pivx/org/pivxwallet/db/SnappyTest.java
@@ -0,0 +1,30 @@
+package pivx.org.pivxwallet.db;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import com.snappydb.SnappydbException;
+
+import org.junit.Test;
+
+import pivx.org.pivxwallet.module.store.AddressStore;
+import pivx.org.pivxwallet.module.store.SnappyStore;
+import pivx.org.pivxwallet.module.store.exceptions.AddressNotFoundException;
+import pivx.org.pivxwallet.module.store.exceptions.CantInsertAddressException;
+
+/**
+ * Created by furszy on 6/15/17.
+ */
+
+public class SnappyTest {
+
+
+    @Test
+    public void insertTest() throws SnappydbException, CantInsertAddressException, AddressNotFoundException {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        AddressStore addressStore = new SnappyStore(appContext);
+        addressStore.insert("yChC1VQS5zET5pDxXgcc4bFye3Q9nurccG","321:dasdmasiodnioen13kelfeqe1");
+        assert addressStore.getAddressStatus("yChC1VQS5zET5pDxXgcc4bFye3Q9nurccG").equals("321:dasdmasiodnioen13kelfeqe1");
+    }
+
+}
---
+++ app/src/main/java/pivx/org/pivxwallet/module/store/SnappyStore.java
@@ -0,0 +1,62 @@
+package pivx.org.pivxwallet.module.store;
+
+import android.content.Context;
+
+import com.snappydb.DB;
+import com.snappydb.DBFactory;
+import com.snappydb.SnappydbException;
+
+import store.AddressNotFoundException;
+import store.AddressStore;
+import store.CantInsertAddressException;
+
+
+/**
+ * Created by furszy on 6/14/17.
+ */
+
+public class SnappyStore implements AddressStore {
+
+    private static final String DB_NAME = "addresses";
+
+    private DB snappyDb;
+
+    public SnappyStore(Context context) throws SnappydbException {
+        snappyDb = DBFactory.open(context,DB_NAME);
+    }
+
+    /**
+     * Insert a base58 address an his status (hash of the address history)
+     *
+     * @param address
+     * @param status
+     */
+    public void insert(String address,String status) throws CantInsertAddressException {
+        try {
+            snappyDb.put(address, status);
+        } catch (SnappydbException e) {
+            e.printStackTrace();
+            throw new CantInsertAddressException("Cant insert: "+address,e);
+        }
+    }
+
+    /**
+     * Get the address status
+     */
+    public String getAddressStatus(String address) throws AddressNotFoundException {
+        try {
+            return snappyDb.get(address);
+        } catch (SnappydbException e) {
+            e.printStackTrace();
+            throw new AddressNotFoundException("Cant insert: "+address,e);
+        }
+    }
+
+
+    public void close() throws SnappydbException {
+        if (snappyDb!=null){
+            snappyDb.close();
+        }
+    }
+
+}
