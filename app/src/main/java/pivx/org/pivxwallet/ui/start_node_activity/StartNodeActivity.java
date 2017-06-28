a1038b7265f90598af8204f2f406d34a075015f1
Gregory Solarte <Neoperol@Gregorys-MacBook-Pro.local>
2017-06-28 01:49:21 -0400

    Dialog Start Node added

--- app/src/main/java/pivx/org/pivxwallet/ui/start_node_activity/StartNodeActivity.java
+++ app/src/main/java/pivx/org/pivxwallet/ui/start_node_activity/StartNodeActivity.java
@@ -1,13 +1,17 @@
 package pivx.org.pivxwallet.ui.start_node_activity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import pivx.org.pivxwallet.R;
 import pivx.org.pivxwallet.ui.base.BaseActivity;
+import pivx.org.pivxwallet.utils.DialogBuilder;
 
 /**
  * Created by Neoperol on 6/27/17.
@@ -16,6 +20,9 @@ import pivx.org.pivxwallet.ui.base.BaseActivity;
 public class StartNodeActivity extends BaseActivity {
     Button openDialog;
     Button btnSelectNode;
+    EditText tcpText;
+    EditText sslText;
+    EditText hostText;
     @Override
     protected void onCreateView(Bundle savedInstanceState, ViewGroup container) {
         getLayoutInflater().inflate(R.layout.fragment_start_node, container);
@@ -23,8 +30,27 @@ public class StartNodeActivity extends BaseActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+
         // Open Dialog
         openDialog = (Button) findViewById(R.id.openDialog);
+        openDialog.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                LayoutInflater content = LayoutInflater.from(StartNodeActivity.this);
+                View dialogView = content.inflate(R.layout.dialog_node, null);
+                DialogBuilder nodeDialog = new DialogBuilder(StartNodeActivity.this);
+                nodeDialog.setTitle("Add your Node");
+                nodeDialog.setView(dialogView);
+                nodeDialog.setPositiveButton("Add Node", null);
+                nodeDialog.setNegativeButton("Cancel", null);
+                nodeDialog.show();
+            }
+
+        });
+
+        tcpText = (EditText) findViewById(R.id.tcpText);
+        sslText = (EditText) findViewById(R.id.sslText);
+        hostText = (EditText) findViewById(R.id.hostText);
 
         // Node selected
         btnSelectNode = (Button) findViewById(R.id.btnSelectNode);
