2fd0cb71d2e1dda7526db0564b7de3176f1e849b
Gregory Solarte <Neoperol@Gregorys-MacBook-Pro.local>
2017-07-10 17:49:53 -0400

    Extending TextView font

---
+++ app/src/main/java/pivx/org/pivxwallet/utils/FontCache.java
@@ -0,0 +1,31 @@
+package pivx.org.pivxwallet.utils;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+import java.util.HashMap;
+
+/**
+ * Created by Neoperol on 7/10/17.
+ */
+
+public class FontCache {
+
+    private static HashMap<String, Typeface> fontCache = new HashMap<>();
+
+    public static Typeface getTypeface(String fontname, Context context) {
+        Typeface typeface = fontCache.get(fontname);
+
+        if (typeface == null) {
+            try {
+                typeface = Typeface.createFromAsset(context.getAssets(), fontname);
+            } catch (Exception e) {
+                return null;
+            }
+
+            fontCache.put(fontname, typeface);
+        }
+
+        return typeface;
+    }
+}
