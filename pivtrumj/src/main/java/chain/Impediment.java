a48370f594c38c4dd74642fafdc901ceb8396689
furszy <mati_fur@hotmail.com>
2018-03-04 11:35:39 -0300

    core submodule added

--- pivtrumj/src/main/java/chain/Impediment.java
+++
@@ -1,5 +0,0 @@
-package chain;
-
-public enum Impediment {
-    STORAGE, NETWORK
-}
--- pivtrumj/src/main/java/global/exceptions/ContactAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package global.exceptions;
-
-/**
- * Created by furszy on 7/11/17.
- */
-
-public class ContactAlreadyExistException extends Exception {
-}
