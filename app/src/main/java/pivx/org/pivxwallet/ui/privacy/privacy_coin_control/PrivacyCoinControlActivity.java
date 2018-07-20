60a17d29efa4faee607b5f12e13a5c48d7b61ad1
Gregory Solarte <rakgpao@gmail.com>
2018-07-20 15:33:02 -0300

    Restore wallet view changed

--- app/src/main/java/pivx/org/pivxwallet/ui/privacy/privacy_coin_control/PrivacyCoinControlActivity.java
+++ app/src/main/java/pivx/org/pivxwallet/ui/privacy/privacy_coin_control/PrivacyCoinControlActivity.java
@@ -1,9 +1,12 @@
 package pivx.org.pivxwallet.ui.privacy.privacy_coin_control;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,6 +19,7 @@ import pivx.org.pivxwallet.R;
 import pivx.org.pivxwallet.ui.base.BaseActivity;
 
 public class PrivacyCoinControlActivity extends BaseActivity {
+    private static final int MENU_ITEM_ITEM1 = 1;
     private TextView text_balance, text_selected;
     private EditText edit_count_demon1, edit_count_demon5, edit_count_demon10, edit_count_demon50, edit_count_demon100, edit_count_demon500, edit_count_demon1000, edit_count_demon5000;
     private TextView text_demon1, text_demon5, text_demon10, text_demon50, text_demon100, text_demon500, text_demon1000, text_demon5000;
@@ -37,5 +41,22 @@ public class PrivacyCoinControlActivity extends BaseActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.darkPurple)));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, MENU_ITEM_ITEM1, Menu.NONE, R.string.save);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_ITEM_ITEM1:
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
